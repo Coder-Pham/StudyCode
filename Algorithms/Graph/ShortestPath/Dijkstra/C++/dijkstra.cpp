1a21f837a4db8c830c153f5284377f2759cd2dea
Nhat Nguyen <nhatnguyen.sg.vn@gmail.com>
2018-11-17 11:18:38 +0700

    Refactor Dijkstra

--- Algorithms/Graph/ShortestPath/Dijkstra/C++/dijkstra.cpp
+++ Algorithms/Graph/ShortestPath/Dijkstra/C++/dijkstra.cpp
@@ -4,39 +4,34 @@
  *  Note: this is the improved version of dijkstra - by implementing priority-queue
 **/
 
-
 #include <algorithm>
 #include <iostream>
 #include <queue>     //std::priority_queue
 #include <vector>
 
-
 using namespace std;
 
-
 struct Edge {
-	int v, w;
-	
-	Edge(int v_, int w_) {
-		v = v_, w = w_;
-	}
-};
+    int v, w;
 
+    Edge(int v_, int w_) {
+        v = v_, w = w_;
+    }
+};
 
 struct Vertex {
-	int label;
-	int d;
-	
-	Vertex(int label_, int d_) {
-		label = label_;
-		d  = d_;
-	}
-	
-	bool operator < (const Vertex& other) const {
-		return d > other.d;   // minheap
-	}
-};
+    int label;
+    int d;
 
+    Vertex(int label_, int d_) {
+        label = label_;
+        d  = d_;
+    }
+
+    bool operator < (const Vertex& other) const {
+        return d > other.d;   // minheap
+    }
+};
 
 const int MAXV = (int) 1e3;
 const int MAXE = (int) 1e3;
@@ -45,8 +40,7 @@ int dist[MAXV];
 bool visited[MAXV];
 vector<Edge> adj[MAXV];
 priority_queue<Vertex, vector<Vertex>> pq;
-int nV, nE; 
-
+int nV, nE;
 
 void relax(int u, Edge e) {
     if (dist[e.v] > dist[u] + e.w) {
@@ -55,42 +49,40 @@ void relax(int u, Edge e) {
     }
 }
 
-
-void dijkstra(int src) {	    
+void dijkstra(int src) {
     fill(dist, dist + nV, INF);
-	fill(visited, visited + nV, false);
+    fill(visited, visited + nV, false);
 
-	dist[src] = 0;
-	pq.emplace(src, 0);
-	
+    dist[src] = 0;
+    pq.emplace(src, 0);
+
     while (!pq.empty()) {
         int u = pq.top().label;
         pq.pop();
         if (visited[u]) continue;
-        visited[u] = true;        
-        for (Edge e : adj[u]) {
+        visited[u] = true;
+        for (Edge& e : adj[u]) {
             if (visited[e.v]) continue;
             relax(u, e);
         }
     }
 }
 
-
 int main() {
     cin >> nV >> nE;
 
     for (int i = 0; i < nE; i++) {
-		int u, v, w;
-		cin >> u >> v >> w;
-		adj[u].emplace_back(v, w);
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].emplace_back(v, w);
     }
-	
-	dijkstra(0);
+
+    dijkstra(0);
 
     for (int i = 0; i < nV; i++) {
         printf("Dist[%d] = %d\n", i, dist[i]);
     }
-    
+
     return(0);
 }
 
@@ -116,4 +108,4 @@ Dist[3] = 1
 Dist[4] = 4
 Dist[5] = 5
 
- */
+*/
